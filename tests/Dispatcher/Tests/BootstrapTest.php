02b75349211fb1b5def486c4f58b8bf0cebc2eea
bcen <bocai.cen@gmail.com>
2012-10-04 08:21:27 -0400

    Object no need to pass by reference

--- tests/Dispatcher/Tests/BootstrapTest.php
+++ tests/Dispatcher/Tests/BootstrapTest.php
@@ -9,7 +9,7 @@ class BootstrapTest extends \PHPUnit_Framework_Testcase
         $this->assertNotEmpty(APPPATH);
         $this->assertNotEmpty(BASEPATH);
         $this->assertTrue(function_exists('get_instance'));
-        $CI =& get_instance();
+        $CI = get_instance();
         $this->assertNotNull($CI);
         $this->assertEquals(CI_HASH, spl_object_hash($CI));
     }
