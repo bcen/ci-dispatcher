47ff7a7bdae06e891a7dce9d42dfec30e358b8ef
bcen <bocai.cen@gmail.com>
2012-10-04 00:56:03 -0400

    Adding tests for BootstrapController

--- tests/Dispatcher/Tests/BootstrapControllerTest.php
+++ tests/Dispatcher/Tests/BootstrapControllerTest.php
@@ -3,34 +3,31 @@ namespace Dispatcher\Tests;
 
 class BootstrapControllerTest extends \PHPUnit_Framework_TestCase
 {
-    /**
-     * @var \Dispatcher\BootstrapController
-     */
-    private $controllerMock;
-
-    public function setUp()
+    public function test__remap_OnNormalControlFlow_ShouldPass()
     {
-        $this->controllerMock = $this->getMock(
-            'Dispatcher\\BootstrapController',
-            array('loadMiddlewares', 'renderResponse', 'dispatch'),
+        $ctrl = $this->getMock('Dispatcher\\BootstrapController',
+            array('loadMiddlewares', 'dispatch', 'renderResponse'),
             array(),
             '',
             false);
 
-        $this->controllerMock
-            ->expects($this->once())
+        $ctrl->expects($this->once())
             ->method('loadMiddlewares')
             ->will($this->returnValue(array()));
 
-        $this->controllerMock
-            ->expects($this->once())
+        $ctrl->expects($this->once())
             ->method('dispatch')
+            ->with($this->equalTo(array('method', 'api', 'v1', 'books')))
             ->will($this->returnValue(\Dispatcher\JsonResponse::create()));
-    }
 
-    public function test__remap_OnBlogIndexController_ShouldPass()
-    {
-        $this->controllerMock->_remap('blog', array());
+        $ctrl->expects($this->once())
+            ->method('renderResponse')
+            ->with($this->isInstanceOf('Dispatcher\\HttpRequestInterface'),
+                   $this->isInstanceOf('Dispatcher\\HttpResponseInterface'));
+
+
+        $ctrl->_remap('method', array('api', 'v1', 'books'));
+
         $this->assertTrue(1 == 1);
     }
 }
