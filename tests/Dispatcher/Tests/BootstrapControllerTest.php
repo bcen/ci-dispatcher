bf33245015a53f5645de996084b663031f3ded21
bcen <bocai.cen@gmail.com>
2012-10-03 23:41:48 -0400

    Start mocking the BootstrapController

--- tests/Dispatcher/Tests/BootstrapControllerTest.php
+++ tests/Dispatcher/Tests/BootstrapControllerTest.php
@@ -3,14 +3,34 @@ namespace Dispatcher\Tests;
 
 class BootstrapControllerTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @var \Dispatcher\BootstrapController
+     */
     private $controllerMock;
 
     public function setUp()
     {
+        $this->controllerMock = $this->getMock(
+            'Dispatcher\\BootstrapController',
+            array('loadMiddlewares', 'renderResponse', 'dispatch'),
+            array(),
+            '',
+            false);
+
+        $this->controllerMock
+            ->expects($this->once())
+            ->method('loadMiddlewares')
+            ->will($this->returnValue(array()));
+
+        $this->controllerMock
+            ->expects($this->once())
+            ->method('dispatch')
+            ->will($this->returnValue(\Dispatcher\JsonResponse::create()));
     }
 
-    public function testTest()
+    public function test__remap_OnBlogIndexController_ShouldPass()
     {
+        $this->controllerMock->_remap('blog', array());
         $this->assertTrue(1 == 1);
     }
 }
