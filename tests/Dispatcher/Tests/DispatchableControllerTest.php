6dd290bbf0ef2d0e248d778e881325e89a93c3f2
bcen <bocai.cen@gmail.com>
2012-10-09 16:47:25 -0400

    Added test case for DispatchableControllerTest.php

--- tests/Dispatcher/Tests/DispatchableControllerTest.php
+++ tests/Dispatcher/Tests/DispatchableControllerTest.php
@@ -8,7 +8,7 @@ class DispatchableControllerTest extends \PHPUnit_Framework_Testcase
      */
     private $controller;
 
-    public function test_GET_OnRequestWithSimpleControllerStub_ShouldReturn200ResponseWithIndexViews()
+    public function test_GET_OnRequest_ShouldReturn200ResponseWithIndexViews()
     {
         $requestMock = $this->getMock('Dispatcher\\HttpRequestInterface');
         $ctrl = $this->getMockForAbstractClass(
@@ -27,5 +27,29 @@ class DispatchableControllerTest extends \PHPUnit_Framework_Testcase
 
         $this->assertEquals(200, $response->getStatusCode());
         $this->assertContains('index', $response->getViews());
+    }
+
+    public function test_doDispatch_OnInvalidRequestMethod_ShouldReturnError404()
+    {
+        $requestMock = $this->getMock('Dispatcher\\HttpRequest',
+            array('getMethod'));
+        $requestMock->expects($this->any())
+            ->method('getMethod')
+            ->will($this->returnValue('SOMETHING'));
+
+        $ctrl = $this->getMockForAbstractClass(
+            'Dispatcher\\DispatchableController',
+            array(),
+            '',
+            true,
+            true,
+            true,
+            array('getViews'));
+        $ctrl->expects($this->never())
+            ->method('getViews')
+            ->will($this->returnValue(array('index')));
+
+        $response = $ctrl->doDispatch($requestMock, array(), true);
+        $this->assertEquals(404, $response->getStatusCode());
     }
 }
