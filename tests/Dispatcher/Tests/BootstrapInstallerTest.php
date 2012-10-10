9012ec561b7131898d209e486e4415e1b6182d25
bcen <bocai.cen@gmail.com>
2012-10-10 10:30:55 -0400

    Minor refactor on BootstrapInstallerTest.php

--- tests/Dispatcher/Tests/BootstrapInstallerTest.php
+++ tests/Dispatcher/Tests/BootstrapInstallerTest.php
@@ -1,12 +1,17 @@
 <?php
 namespace Dispatcher\Tests;
 
+use Dispatcher\BootstrapInstaller;
+
 class BootstrapInstallerTest extends \PHPUnit_Framework_Testcase
 {
-    public function test_run_ShouldAlterRoute()
+    /**
+     * @test
+     */
+    public function run_OnRouteWithoutInstallation_ShouldAlterRouteWithDefaultController()
     {
         $route = array();
-        \Dispatcher\BootstrapInstaller::run($route, TRUE);
+        BootstrapInstaller::run($route, true);
         $this->assertTrue(isset($route['default_controller'], $route['(.*)']));
     }
 }
