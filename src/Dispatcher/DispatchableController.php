b0e40bdd24e5f8548de4754d23c82add466430f3
bcen <bocai.cen@gmail.com>
2012-10-11 12:10:07 -0400

    Refactored DispatchableController.php and its test

--- src/Dispatcher/DispatchableController.php
+++ src/Dispatcher/DispatchableController.php
@@ -2,38 +2,16 @@
 namespace Dispatcher;
 
 /**
- * Base class of controller for Dispatcher.
+ * Base controller that implemented {@link \Dispatcher\DispatchableInterface}.
  */
 abstract class DispatchableController implements DispatchableInterface
 {
-    protected $views = '';
-
-    public function __construct()
-    {
-    }
-
-    public function __get($key)
-    {
-        // Remaps CodeIgniter's property to this controller
-        $CI =& get_instance();
-        if (property_exists($CI, $key)) {
-            return $CI->$key;
-        }
-
-        // stolen from
-        // http://www.php.net/manual/en/language.oop5.overloading.php#object.get
-        $trace = debug_backtrace();
-        trigger_error(
-            'Undefined property via __get(): ' . $key .
-            ' in ' . $trace[0]['file'] .
-            ' on line ' . $trace[0]['line'],
-            E_USER_NOTICE);
-
-        return NULL;
-    }
-
     /**
-     * {@inheritdoc}
+     * Does the actual dispatching.
+     * @param HttpRequestInterface $request
+     * @param array $params
+     * @return HttpResponseInterface
+     * @throws \LogicException When there is not enough expected arguments
      */
     public function doDispatch(HttpRequestInterface $request,
                                array $params = array())
@@ -56,33 +34,48 @@ abstract class DispatchableController implements DispatchableInterface
         return $response;
     }
 
+    /**
+     * The default handler for GET request.
+     * @param HttpRequestInterface $request
+     * @return HttpResponseInterface
+     */
     public function get(HttpRequestInterface $request)
     {
         $data = call_user_func_array(
-            array($this, 'getContextData'),
-            func_get_args());
+            array($this, 'getContextData'), func_get_args());
         return $this->renderView($data);
     }
 
     /**
-     * Returns data according to the current context.
-     * @return array the data
+     * Gets the current context data for response.
+     * @return array The data in array
      */
     public function getContextData()
     {
         return array();
     }
 
+    /**
+     * Gets the views for view template response.
+     * @return array
+     * @throws \LogicException When there is no views property defined
+     */
     protected function getViews()
     {
-        $views = is_array($this->views) ? $this->views : array($this->views);
+        $views = property_exists($this, 'views') ? $this->views : array();
+        $views = is_array($views) ? $views : array($views);
         if (empty($views)) {
-            show_error('Declare your views as protected ' .
-                       '$views = array("index") or "index"');
+            throw new \LogicException('No views defined.');
         }
         return $views;
     }
 
+    /**
+     * Creates a {@link \Dispatcher\ViewTemplateResponse}.
+     * @param array $data
+     * @param int $statusCode
+     * @return HttpResponseInterface
+     */
     protected function renderView(array $data = array(), $statusCode = 200)
     {
         return ViewTemplateResponse::create($statusCode)
@@ -90,12 +83,24 @@ abstract class DispatchableController implements DispatchableInterface
             ->setViews($this->getViews());
     }
 
+    /**
+     * Creates a {@link \Dispatcher\RawHtmlResponse}.
+     * @param string $html
+     * @param int $statusCode
+     * @return HttpResponseInterface
+     */
     protected function renderHtml($html = '', $statusCode = 200)
     {
         return RawHtmlResponse::create($statusCode, $html);
     }
 
-    protected function renderJson($data = NULL, $statusCode = 200)
+    /**
+     * Creates a {@link \Dispatcher\JsonResponse}.
+     * @param mixed $data
+     * @param int $statusCode
+     * @return HttpResponseInterface
+     */
+    protected function renderJson($data = null, $statusCode = 200)
     {
         return JsonResponse::create($statusCode)->setData($data);
     }
--- tests/Dispatcher/Tests/DispatchableControllerTest.php
+++ tests/Dispatcher/Tests/DispatchableControllerTest.php
@@ -6,17 +6,12 @@ class DispatchableControllerTest extends \PHPUnit_Framework_Testcase
     /**
      * @test
      */
-    public function GET_WithIndexViewAndData_ShouldReturnViewTemplateResponse()
+    public function get_WithViewAndData_ShouldReturnViewTemplateResponseWithSameViewAndData()
     {
         $requestMock = $this->getMock('Dispatcher\\HttpRequestInterface');
 
-        $controller = $this->getMockForAbstractClass(
+        $controller = $this->getMock(
             'Dispatcher\\DispatchableController',
-            array(),
-            '',
-            true,
-            true,
-            true,
             array('getViews', 'getContextData'));
         $controller->expects($this->once())
             ->method('getViews')
@@ -35,29 +30,41 @@ class DispatchableControllerTest extends \PHPUnit_Framework_Testcase
 
     /**
      * @test
+     * @expectedException LogicException
+     * @expectedExceptionMessage No views defined.
+     */
+    public function get_WithoutView_ShouldThrowLogicException()
+    {
+        $requestMock = $this->getMock('Dispatcher\\HttpRequestInterface');
+
+        $controller = $this->getMock(
+            'Dispatcher\\DispatchableController', array('getContextData'));
+        $controller->expects($this->once())
+            ->method('getContextData')
+            ->will($this->returnValue(array('message' => 'Hey')));
+
+        $controller->get($requestMock);
+    }
+
+    /**
+     * @test
      * @expectedException ReflectionException
      */
-    public function doDispatch_OnInvalidRequestMethod_ShouldReturnError404()
+    public function doDispatch_OnInvalidRequestMethod_ShouldThrowReflectionException()
     {
-        $request = $this->getMock('Dispatcher\\HttpRequest',
+        $requestMock = $this->getMock('Dispatcher\\HttpRequest',
             array('getMethod'), array(get_instance()));
-        $request->expects($this->any())
+        $requestMock->expects($this->any())
             ->method('getMethod')
             ->will($this->returnValue('POST'));
 
-        $controller = $this->getMockForAbstractClass(
-            'Dispatcher\\DispatchableController',
-            array(),
-            '',
-            true,
-            true,
-            true,
-            array('getViews'));
+        $controller = $this->getMock(
+            'Dispatcher\\DispatchableController', array('getViews'));
         $controller->expects($this->never())
             ->method('getViews')
             ->will($this->returnValue(array('index')));
 
-        $controller->doDispatch($request, array(), true);
+        $controller->doDispatch($requestMock, array());
     }
 
     /**
@@ -65,29 +72,42 @@ class DispatchableControllerTest extends \PHPUnit_Framework_Testcase
      */
     public function doDispatch_OnValidRequestMethod_ShouldReturnValidResponse()
     {
-        $request = $this->getMock('Dispatcher\\HttpRequest',
+        $requestMock = $this->getMock('Dispatcher\\HttpRequest',
             array('getMethod'), array(get_instance()));
-        $request->expects($this->any())
+        $requestMock->expects($this->any())
             ->method('getMethod')
             ->will($this->returnValue('GET'));
 
-        $controller = $this->getMockForAbstractClass(
-            'Dispatcher\\DispatchableController',
-            array(),
-            '',
-            true,
-            true,
-            true,
-            array('getViews', 'get'));
-        $controller->expects($this->never())
-            ->method('getViews')
-            ->will($this->returnValue(array('index')));
+        $controller = $this->getMock(
+            'Dispatcher\\DispatchableController', array('get'));
         $controller->expects($this->once())
             ->method('get')
             ->with($this->isInstanceOf('Dispatcher\\HttpRequestInterface'))
             ->will($this->returnValue(\Dispatcher\JsonResponse::create()));
 
-        $response = $controller->doDispatch($request, array(), true);
+        $response = $controller->doDispatch($requestMock, array());
         $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    /**
+     * @test
+     * @expectedException LogicException
+     */
+    public function doDispatch_WithoutExpectedParams_ShouldThrowLogicException()
+    {
+        $requestMock = $this->getMock('Dispatcher\\HttpRequest',
+            array('getMethod'), array(get_instance()));
+        $requestMock->expects($this->any())
+            ->method('getMethod')
+            ->will($this->returnValue('POST'));
+
+        $controller = $this->getMock(
+            'Dispatcher\\DispatchableController', array('post'));
+        $controller->expects($this->any())
+            ->method('post')
+            ->with($this->isInstanceOf('Dispatcher\\HttpRequestInterface'))
+            ->will($this->returnValue(\Dispatcher\JsonResponse::create()));
+
+        $controller->doDispatch($requestMock, array());
     }
 }
