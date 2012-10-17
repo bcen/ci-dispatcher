ec747e5721fe55173954507e1a8e66ce3074f058
bcen <bocai.cen@gmail.com>
2012-10-17 09:10:10 -0400

    Take one test case off for refactoring

--- tests/Dispatcher/Tests/DispatchableResourceTest.php
+++ tests/Dispatcher/Tests/DispatchableResourceTest.php
@@ -6,32 +6,6 @@ class DispatchableResourceTest extends \PHPUnit_Framework_Testcase
     /**
      * @test
      */
-    public function doDispatch_WithNoGetHandler_ShouldThrowDispatchingExceptionWith501NotImplementedResponse()
-    {
-        $reqMock = $this->getMock('Dispatcher\\HttpRequest',
-            array('getMethod'));
-        $reqMock->expects($this->any())
-            ->method('getMethod')
-            ->will($this->returnValue('GET'));
-
-        $controller = $this->getMock('Dispatcher\\DispatchableResource',
-            array('dummy'));
-        $controller->expects($this->any())
-            ->method('dummy');
-
-        try {
-            $controller->doDispatch($reqMock);
-        } catch (\Dispatcher\DispatchingException $ex) {
-            $this->assertEquals(501, $ex->getResponse()->getStatusCode());
-            return;
-        }
-
-        $this->fail('Expects \\Dispatcher\\DispatchingException');
-    }
-
-    /**
-     * @test
-     */
     public function doDispatch_WithNoGetMethodAllowed_ShouldThrowDispatchingExceptionWith405NotAllowedResponse()
     {
         $reqMock = $this->getMock('Dispatcher\\HttpRequest',
