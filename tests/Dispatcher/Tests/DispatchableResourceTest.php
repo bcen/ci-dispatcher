cfeb2461d725f19d2289927262c0a82fdb3ceb38
bcen <bocai.cen@gmail.com>
2012-10-22 18:02:11 -0400

    Deleted a useless test case

--- tests/Dispatcher/Tests/DispatchableResourceTest.php
+++ tests/Dispatcher/Tests/DispatchableResourceTest.php
@@ -34,26 +34,4 @@ class DispatchableResourceTest extends \PHPUnit_Framework_TestCase
             '{"meta":{"offset":0,"limit":20,"total":2},"objects":[{"username":"someone"},{"username":"someoneelse"}]}',
             $response->getContent());
     }
-
-    /**
-     * @test
-     */
-    public function get_WithNoUriSegments_ShouldCallReadCollectionWithEmptyResults()
-    {
-        $reqMock = $this->mockRequest('GET');
-
-        $controller = $this->getMock('Dispatcher\\DispatchableResource',
-            array('readCollection'));
-        $controller->expects($this->any())
-            ->method('readCollection')
-            ->with($this->isInstanceOf(
-            'Dispatcher\\Http\\HttpRequestInterface'))
-            ->will($this->returnValue(array()));
-
-        $response = $controller->get($reqMock);
-
-        $this->assertEquals(
-            '{"meta":{"offset":0,"limit":20,"total":0},"objects":[]}',
-            $response->getContent());
-    }
 }
