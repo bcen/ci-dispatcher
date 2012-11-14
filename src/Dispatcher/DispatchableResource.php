df48fb19fbf3b79ade367c625de331eae5a7f674
bcen <bocai.cen@gmail.com>
2012-11-14 10:56:36 -0500

    Added xml serialization

--- src/Dispatcher/DispatchableResource.php
+++ src/Dispatcher/DispatchableResource.php
@@ -327,8 +327,57 @@ abstract class DispatchableResource implements DispatchableInterface
     protected function applySerializationOn(ResourceBundle $bundle, $contentType)
     {
         $data = $bundle->getData();
-        $data = (is_array($data) || is_object($data))
-            ? json_encode($data) : '';
+
+        if (strtolower($contentType) === 'application/xml') {
+            // stolen from codeigniter-restserver
+            // https://github.com/philsturgeon/codeigniter-restserver/blob/master/application/libraries/Format.php#L87
+            $toXml = function($data = null, $structure = null, $basenode = 'response') use(&$toXml) {
+                if (ini_get('zend.ze1_compatibility_mode') == 1) {
+                    ini_set('zend.ze1_compatibility_mode', 0);
+                }
+                if ($structure === null) {
+                    $structure = simplexml_load_string(
+                        "<?xml version='1.0' encoding='utf-8'?><$basenode />");
+                }
+
+                if (!is_array($data) && !is_object($data)) {
+                    $data = (array)$data;
+                }
+
+                foreach ($data as $k => $v) {
+                    if (is_bool($v)) {
+                        $v = $v ? 'true' : 'false';
+                    }
+
+                    if (is_numeric($k)) {
+                        $k = 'item';
+                    }
+
+                    $k = preg_replace('/[^a-z_\-0-9]/i', '', $k);
+
+                    if (is_array($v) || is_object($v)) {
+                        $node = $structure->addChild($k);
+                        $toXml($v, $node, $k);
+                    } else {
+                        $v = htmlspecialchars(
+                            html_entity_decode($v, ENT_QUOTES, 'UTF-8'),
+                            ENT_QUOTES,
+                            "UTF-8"
+                        );
+
+                        $structure->addChild($k, $v);
+                    }
+                }
+
+                return $structure->asXML();
+            };
+
+            $data = $toXml($data);
+        } else {
+            $data = (is_array($data) || is_object($data))
+                ? json_encode($data) : '';
+        }
+
         $bundle->setData($data);
     }
 
